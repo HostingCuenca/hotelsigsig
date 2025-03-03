4704557e80e04bf856b9c7e024a6b3a06dda630c
roggrt <42749056+roggrt@users.noreply.github.com>
2025-03-03 16:07:32 -0500

    correcion commit - subiendo mi proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +18,8 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    historyApiFallback: true, // Esto permite que todas las rutas se redirijan a index.html
   },
 });
