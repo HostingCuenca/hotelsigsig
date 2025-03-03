64ac690b9881bd0624052dd523d5c36d1e7a1c52
roggrt <42749056+roggrt@users.noreply.github.com>
2025-03-03 16:20:07 -0500

    correcion commit - subiendo mi proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,11 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
-// import './index.css';
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
-
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+
+import { Analytics } from "@vercel/analytics/react"
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -24,6 +14,7 @@ createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
